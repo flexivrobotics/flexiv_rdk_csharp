ab2a79c5cf1f6f33bb2196915d0173958f971547
zlin-flexiv <zongjie.lin@flexiv.com>
2025-05-09 13:23:03 +0800

    fix fault function error

--- FlexivRobotDll/FlexivRobotDll.cpp
+++ FlexivRobotDll/FlexivRobotDll.cpp
@@ -9,6 +9,7 @@
 #include <map>
 #include <vector>
 #include <array>
+#include <thread>
 
 #include <comdef.h>
 #include <comutil.h>
@@ -219,7 +220,9 @@ EXPORT_API void DeleteFlexivWork(flexiv::rdk::WorkCoord* work) {
 }
 
 EXPORT_API bool fault(flexiv::rdk::Robot* robot) {
-	return robot->fault();
+	bool flag = robot->fault();
+	std::this_thread::sleep_for(std::chrono::milliseconds(50));
+	return flag;
 }
 
 EXPORT_API bool ClearFault(flexiv::rdk::Robot* robot, int timeout_sec, FlexivError* error) {
