b5950070b7d54b3428e8854ce7134fbd41d8ff46
zlin-flexiv <167504992+zlin-flexiv@users.noreply.github.com>
2026-09-23 16:31:39 +0800

    update to v1.9.2
    This reverts commit 43bc91bd50428347ac4c91666886f6b184d73367.

--- csharp/FlexivRdk/Native.cs
+++ csharp/FlexivRdk/Native.cs
@@ -41,7 +41,10 @@ namespace FlexivRdk
         public static extern int GetMode(IntPtr robot);
 
         [DllImport(k_flexivRdkDll, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void GetStates(IntPtr robot, ref RobotStates robot_state);
+        public static extern void GetStates(IntPtr robot, ref RobotStates robot_state, ref FlexivError error);
+
+        [DllImport(k_flexivRdkDll, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void GetActions(IntPtr robot, ref RobotActions robotActions, ref FlexivError error);
 
         [DllImport(k_flexivRdkDll, CallingConvention = CallingConvention.Cdecl)]
         public static extern int IsStopped(IntPtr robot);
